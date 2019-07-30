d0f261785e535a2bc293f0b8fac04e7379e4a5c2
mjwells2002 <wellsmatthew2002@gmail.com>
2019-07-30 18:26:22 +0100

    dont use package package bad

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const nslookup = require('nslookup');
+var dns = require('dns');
 
 function lookup(){
     let docker = JSON.parse( Buffer.from(process.env.baseConfig,'base64') );
     console.log(docker)
-    nslookup(`tasks.${docker.service_name}`)
-        .server(docker.docker_dns)
-        .type(docker.lookup_type)
-        .end((err,data) => {
-            console.log(data);
-            lookup();
-        })
+    dns.lookup(`tasks.${docker.service_name}`, (a,b) => {
+        console.log(b);
+        setTimeout(lookup,2500);
+    })
 }
 
 lookup();
