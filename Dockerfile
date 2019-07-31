c6f1538efab4384e18ae17880ba43eae490ec2a9
mjwells2002 <wellsmatthew2002@gmail.com>
2019-07-31 01:10:01 +0100

    didnt work go back

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM node:current-alpine
 WORKDIR /app
 COPY . .
-
-FROM arm32v6/debian:stretch-slim 
-COPY qemu-arm-static /usr/bin
-
 ENV baseConfig ewogICAgInNlcnZpY2VfbmFtZSI6ICJhcHAiLAogICAgImRvY2tlcl9kbnMiOiAiMTI3LjAuMC4xMSIsCiAgICAibG9va3VwX3R5cGUiOiAibnMiCn0=
 RUN npm install
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const nslookup = require('nslookup');
+const express = require('express');
 var dns = require('dns');
 
-function lookup(){
+const app = express()
+const port = 3000
+
+app.get('/', lookup)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+function lookup(req,res){
     let docker = JSON.parse( Buffer.from(process.env.baseConfig,'base64') );
     console.log(docker)
     dns.resolve(`tasks.${docker.service_name}`, (a,b) => {
+        res.send(`${process.env.HOSTNAME} -- ${b}`)
         console.log(b);
-        setTimeout(lookup,2500);
+        //setTimeout(lookup,2500);
     })
 }
 
-lookup();
