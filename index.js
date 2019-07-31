f9583e4697719c5c1bbeaf1061ad2f954765cea8
mjwells2002 <wellsmatthew2002@gmail.com>
2019-07-31 01:41:21 +0100

    idk if this works

--- index.js
+++ index.js
@@ -1,21 +1,73 @@
 const express = require('express');
 var dns = require('dns');
+const os = require('os')
+const request = require('request');
 
-const app = express()
-const port = 3000
+const publicApp = express()
+const externalPort = 3000
 
-app.get('/', lookup)
+const internalApp = express()
+const internalPort = 4040
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let appShared = 'lolcatz'
+const myip = os.networkInterfaces()['eth0'][0].address
 
+publicApp.get('/', lookup);
+publicApp.get('/set',extUpdate);
+internalApp.get('/', intApp)
+internalApp.get('/update', post)
+
+publicApp.listen(externalPort, onStart);
+internalApp.listen(internalPort, onStart);
+
+function onStart() {
+    console.log('startred')
+}
 
 function lookup(req,res){
     let docker = JSON.parse( Buffer.from(process.env.baseConfig,'base64') );
     console.log(docker)
     dns.resolve(`tasks.${docker.service_name}`, (a,b) => {
-        res.send(`${process.env.HOSTNAME} -- ${b}`)
+        res.send(`${process.env.HOSTNAME} - ${myip} -- ${b} --${appShared}`)
         console.log(b);
         //setTimeout(lookup,2500);
     })
 }
 
+function intApp(req,res){
+    appShared = req.params.d
+    t3();
+    console.log(`update for PUBLIC appShared ${appShared}`);
+    res.send('OKAY');
+}
+
+function post(req,res){
+    appShared = req.params.d
+    console.log(`update for appShared ${appShared}`);
+    res.send('OKAY');
+}
+
+function t2(callback){
+    let docker = JSON.parse( Buffer.from(process.env.baseConfig,'base64') );
+    console.log(docker)
+    dns.resolve(`tasks.${docker.service_name}`, (a,b) => {
+        //res.send(`${process.env.HOSTNAME} -- ${b}`)
+        callback(b)
+        //setTimeout(lookup,2500);
+    })
+}
+
+function t3(){
+    t2(arr => {
+        arr.forEach(x => {
+            if (x == myip){
+
+            } else {
+                //not only present node
+                request(`http://${ip}:${internalPort}/update?d=${appShared}`, function (error, response, body) {
+                
+                });
+            }
+        })
+    })
+}
